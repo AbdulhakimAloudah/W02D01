ff2af79464c6777db372f4289a880c302bbf8740
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-10-17 13:12:53 +0300

    t1

---
+++ index.js
@@ -0,0 +1,36 @@
+const sum = function (n1, n2) {
+   let sum = n1 + n2;
+ return sum ;
+}
+console.log(sum(5, 10));
+
+sum(15, 10);
+
+//-----
+
+const average = function (a, b) {
+    let average1 = a + b;
+    return average2 / 2
+  };
+  
+  average2(20, 5); // => 12.5
+  average(10, 20); // => 15
+
+
+
+//-----
+/*  
+   5! = 5 * 4 * 3 * 2 * 1
+ */
+
+   const findFactorial = (num) => {
+    // YOUR CODE HERE
+       }
+    /*
+    Examples:
+    findFactorial(1) // => 1
+    findFactorial(3) // => 6
+    findFactorial(4) // => 24
+    findFactorial(6) // => 720
+    findFactorial(11) // => 39916800
+    */
